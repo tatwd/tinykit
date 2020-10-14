9b57747599ffb5311addc2efa9dd313c8632b663
tatwd <leoking9641@hotmail.com>
2020-10-14 23:56:54 +0800

    fix: fix `timeSpan` compute years value

--- src/timeSpan.js
+++ src/timeSpan.js
@@ -1,15 +1,15 @@
 /**
- * 计算时间过去多久(包含不同维度)
- * @param {Date} time 时间
+ * 计算两个时间差(包含不同维度)
+ * @param {Date} atime - 时间 A
+ * @param {Date} [btime] - 时间 B, 可选， 默认值 `new Date()`
  */
-function timeSpan(time) {
-  var now = new Date();
-  var milliseconds = now - time;
-  var days = ~~(milliseconds / (1 * 24 * 60 * 60 * 1000));
-  var hours = ~~(milliseconds / (1 * 60 * 60 * 1000));
-  var minutes = ~~(milliseconds / (1 * 60 * 1000));
+function timeSpan(atime, btime) {
+  var milliseconds = (btime || new Date()) - atime;
   var seconds = ~~(milliseconds / 1000);
-  var years = ~~(milliseconds / 365.5);
+  var minutes = ~~(milliseconds / (1 * 60 * 1000));
+  var hours = ~~(milliseconds / (1 * 60 * 60 * 1000));
+  var days = ~~(milliseconds / (1 * 24 * 60 * 60 * 1000));
+  var years = ~~(days / 365.5);
   return { years, days, hours, minutes, seconds, milliseconds };
 }
 
--- src/timeSpan.test.js
+++ src/timeSpan.test.js
@@ -1,6 +1,6 @@
 import timeSpan from './timeSpan';
 
-test('timeSpan test', () => {
+test('test timeSpan return value', () => {
   var span = timeSpan(new Date('2020-10-14 12:00'));
   expect(span).toHaveProperty('years');
   expect(span).toHaveProperty('days');
@@ -8,4 +8,19 @@ test('timeSpan test', () => {
   expect(span).toHaveProperty('minutes');
   expect(span).toHaveProperty('seconds');
   expect(span).toHaveProperty('milliseconds');
+});
+
+test('test timeSpan two date time', () => {
+  var span = timeSpan(
+    new Date('2020-01-01 12:00'),
+    new Date('2020-10-01 12:00')
+  );
+  expect(span).toStrictEqual({
+    years: 0,
+    days: 274,
+    hours: 6576,
+    minutes: 394560,
+    seconds: 23673600,
+    milliseconds: 23673600000,
+  });
 });
