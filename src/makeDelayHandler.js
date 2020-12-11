a79b65bcd75a1ee63345c3dcca31693b1848ed7a
tatwd <leoking9641@hotmail.com>
2020-12-12 01:06:08 +0800

    chore: add comments

--- src/makeDelayHandler.js
+++ src/makeDelayHandler.js
@@ -1,12 +1,16 @@
+/**
+ * 创建一个延迟执行的处理函数
+ * @param {function} callback 回调函数
+ * @param {number} delay 延迟毫秒数, 可选, 默认值 `800`
+ */
 function makeDelayHandler(callback, delay = 800) {
   let lastTimestamp = undefined;
   return function (args) {
     let id = setTimeout(() => callback && callback(args), delay);
     const nowTimestamp = Date.now();
-    console.log(nowTimestamp - lastTimestamp);
     if (lastTimestamp && nowTimestamp - lastTimestamp < delay) {
+      // 在 delay 时间内的立即清除
       clearTimeout(id);
-      console.log('clear');
     }
     lastTimestamp = nowTimestamp;
   };
