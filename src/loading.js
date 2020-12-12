4c9d14b80453bff3b165c951e8ca6deb7a04bb73
tatwd <leoking9641@hotmail.com>
2020-12-12 19:41:50 +0800

    refactor(loading): update

--- src/loading.js
+++ src/loading.js
@@ -4,7 +4,9 @@
  * @param {object} option 配置项
  */
 function loading(callback, option) {
-  const { el = document.body, html = 'Loading...', delay = 500 } = option;
+  const el = option.el || document.body;
+  const html = option.html || 'Loading...';
+  const delay = option.delay || 500;
   const state = {
     target: el,
   };
