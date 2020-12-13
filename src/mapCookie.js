b0511fd26a39e5eb0079130d03615b38507859d0
tatwd <leoking9641@hotmail.com>
2020-12-13 19:33:31 +0800

    fix: update `mapCookie` regex

--- src/mapCookie.js
+++ src/mapCookie.js
@@ -2,13 +2,16 @@
  * 获取 cookie 映射对象
  */
 function mapCookie() {
-  const cookies = document.cookie.trim().split(/=|;\s|;/);
-  if (!cookies.length) return {};
-  const len = cookies.length - (cookies.length & 1);
+  const parts = document.cookie.trim().split(/;\s?/);
+  if (!parts.length) return {};
+  const len = parts.length;
   const cookieMap = {};
-  for (let i = 0; i < len; i += 2) {
-    const key = cookies[i];
-    const val = decodeURIComponent(cookies[i + 1]);
+  for (let i = 0; i < len; ++i) {
+    const part = parts[i];
+    const idx = part.indexOf('=');
+    if (idx == -1) continue;
+    const key = part.substring(0, idx);
+    const val = decodeURIComponent(part.substring(idx + 1));
     cookieMap[key] = val;
   }
   return cookieMap;
