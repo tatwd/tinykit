38face6d1ba0b794f387c7aca838b4dc5e24157f
tatwd <leoking9641@hotmail.com>
2021-02-04 22:47:40 +0800

    chore: add func bundle config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
+import { readdirSync, statSync } from 'fs';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
-export default [
+const bundles = [
   {
     input: 'index.js',
     output: [
@@ -30,3 +31,43 @@ export default [
     plugins: [resolve(), babel({ babelHelpers: 'bundled' })],
   },
 ];
+
+const createBundle = (func) => {
+  const inputF = './src/' + func + '.js';
+  const outputF = 'dist/x/' + func;
+  return {
+    input: inputF,
+    output: [
+      { file: outputF + '.js', format: 'iife', name: func },
+      {
+        file: outputF + '.min.js',
+        name: func,
+        format: 'iife',
+        sourcemap: true,
+        plugins: [terser()],
+      },
+    ],
+    plugins: [resolve(), babel({ babelHelpers: 'bundled' })],
+  };
+};
+
+const ignoreList = [];
+const singles = [];
+
+readdirSync('./src').forEach((f) => {
+  if (!f.endsWith('.js')) return;
+  if (f.endsWith('.test.js')) return;
+  const fullname = __dirname + '/src/' + f;
+  if (statSync(fullname).isDirectory()) return;
+  const filename = f.split('.js')[0];
+  if (ignoreList.includes(filename)) return;
+  singles.push(filename);
+});
+
+for (const f of singles) {
+  if (!f) continue;
+  // @ts-ignore
+  bundles.push(createBundle(f));
+}
+
+export default bundles;
