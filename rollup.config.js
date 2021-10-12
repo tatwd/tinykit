446cadff9efbe8806b75f8ba6118bb9509cf4c8e
tatwd <leoking9641@hotmail.com>
2021-10-12 22:10:46 +0800

    chore: update bundle config

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,42 @@
-import { readdirSync, statSync } from 'fs';
+import { readdirSync, statSync, writeFileSync } from 'fs';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+import { basename, join } from 'path';
 
-const bundles = [
-  {
-    input: 'index.js',
-    output: [
-      {
-        file: 'dist/bundle.cjs.js',
-        format: 'cjs',
-      },
-      {
-        file: 'dist/bundle.esm.js',
-        format: 'esm',
-      },
-      {
-        file: 'dist/bundle.umd.js',
-        format: 'umd',
-        name: 'tinykit',
-      },
-      {
-        file: 'dist/bundle.umd.min.js',
-        sourcemap: true,
-        format: 'umd',
-        name: 'tinykit',
-        plugins: [terser()],
-      },
-    ],
-    plugins: [resolve(), babel({ babelHelpers: 'bundled' })],
-  },
-];
+function readModules() {
+  const ignoreList = [];
+  const modList = [];
 
-const createBundle = (func) => {
-  const inputF = './src/' + func + '.js';
-  const outputF = 'dist/x/' + func;
+  readdirSync('./src').forEach((f) => {
+    if (!f.endsWith('.js')) return;
+    if (f.endsWith('.test.js')) return;
+
+    const fullname = join(__dirname, 'src', f);
+    if (statSync(fullname).isDirectory()) return;
+
+    const funcName = basename(f, '.js');
+    if (ignoreList.includes(funcName)) return;
+
+    modList.push({
+      path: fullname,
+      func: funcName,
+    });
+  });
+
+  return modList;
+}
+
+function createBundle(mod) {
+  const inputF = mod.path;
+  const outputF = 'dist/x/' + mod.func;
   return {
     input: inputF,
     output: [
-      { file: outputF + '.js', format: 'iife', name: func },
+      { file: outputF + '.js', format: 'iife', name: mod.func },
       {
         file: outputF + '.min.js',
-        name: func,
+        name: mod.func,
         format: 'iife',
         sourcemap: true,
         plugins: [terser()],
@@ -49,25 +44,56 @@ const createBundle = (func) => {
     ],
     plugins: [resolve(), babel({ babelHelpers: 'bundled' })],
   };
-};
-
-const ignoreList = [];
-const singles = [];
-
-readdirSync('./src').forEach((f) => {
-  if (!f.endsWith('.js')) return;
-  if (f.endsWith('.test.js')) return;
-  const fullname = __dirname + '/src/' + f;
-  if (statSync(fullname).isDirectory()) return;
-  const filename = f.split('.js')[0];
-  if (ignoreList.includes(filename)) return;
-  singles.push(filename);
-});
-
-for (const f of singles) {
-  if (!f) continue;
-  // @ts-ignore
-  bundles.push(createBundle(f));
 }
 
-export default bundles;
+function generateBundles() {
+  const entryFile = './index.js';
+  const modList = readModules();
+
+  // generate entry file
+  let indexJsContent = '// Auto generate by rollup.config.js';
+  for (const mod of modList) {
+    indexJsContent += `
+export { default as ${mod.func} } from './src/${mod.func}';`;
+  }
+
+  writeFileSync(entryFile, indexJsContent);
+
+  const bundles = [
+    {
+      input: entryFile,
+      output: [
+        {
+          file: 'dist/bundle.cjs.js',
+          format: 'cjs',
+        },
+        {
+          file: 'dist/bundle.esm.js',
+          format: 'esm',
+        },
+        {
+          file: 'dist/bundle.umd.js',
+          format: 'umd',
+          name: 'tinykit',
+        },
+        {
+          file: 'dist/bundle.umd.min.js',
+          sourcemap: true,
+          format: 'umd',
+          name: 'tinykit',
+          plugins: [terser()],
+        },
+      ],
+      plugins: [resolve(), babel({ babelHelpers: 'bundled' })],
+    },
+  ];
+
+  for (const mod of modList) {
+    // @ts-ignore
+    bundles.push(createBundle(mod));
+  }
+
+  return bundles;
+}
+
+export default generateBundles();
